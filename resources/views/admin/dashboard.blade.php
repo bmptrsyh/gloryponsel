98e3772dcf7594736c9b4257b4916beefd5c5fdf
bmptrsyh <bimaputrsyah320@gmail.com>
2025-04-11 09:57:01 +0700

    c

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -102,6 +102,7 @@
                     pointBorderWidth: 2,
                 }]
             },
+            // chart
             options: {
                 responsive: true,
                 scales: {
